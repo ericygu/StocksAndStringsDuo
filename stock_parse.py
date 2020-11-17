2859a706951b9e9dd41e0aa91548a35be64cece7
ericygu <gudmaneric@gmail.com>
2020-11-16 23:22:28 -0500

    Fixed Stock Parse

--- stock_parse.py
+++ stock_parse.py
@@ -7,58 +7,56 @@ import datetime
 import requests
 from load_articles import read_articles, write_articles
 
-APIKey = "442ONKXSVHA79170"  # redact in submissions
-APIbase = "https://www.alphavantage.co/query"
-
-
-def rqstStockTSDataDaily(sym):
-    r = None
-    tries = 0
-    maxTries = 10
-    while not r and tries < maxTries:
-        r = requests.get(APIbase, params={
-                         "function": "TIME_SERIES_DAILY", "symbol": sym, "outputsize": "full", "apikey": APIKey})
-        tries += 1
-
-    if not r:
-        raise ValueError("Something unexpected happened.")
-
-    return r.json()["Time Series (Daily)"]
-
-
-def getTSDataDaily(stockDat, t):
-    try:
-        e = stockDat[t.strftime("%Y-%m-%d")]
-        return {"open": round(float(e["1. open"]), 2),
-                "close": round(float(e["4. close"]), 2),
-                "high": round(float(e["2. high"]), 2),
-                "low": round(float(e["3. low"]), 2),
-                "volume": int(e["5. volume"])}
-    except KeyError:
-        return False
-
-
-def getTSDataDailyForceSuccessFuture(stockDat, t, maxFail):
-    y = getTSDataDaily(stockDat, t)
-    i = 0
-    while not y:
-        t += datetime.timedelta(days=1)
-        y = getTSDataDaily(stockDat, t)
-        i += 1
-        if i == maxFail:
-            return False
-
-    return (t, y)
-
 
 def updateJSON_prices(sym):
+
+    APIKey = "442ONKXSVHA79170"  # redact in submissions
+    APIbase = "https://www.alphavantage.co/query"
+
+    def rqstStockTSDataDaily(sym):
+        r = None
+        tries = 0
+        maxTries = 10
+        while not r and tries < maxTries:
+            r = requests.get(APIbase, params={
+                "function": "TIME_SERIES_DAILY", "symbol": sym, "outputsize": "full", "apikey": APIKey})
+            tries += 1
+
+        if not r:
+            raise ValueError("Something unexpected happened.")
+
+        return r.json()["Time Series (Daily)"]
+
+    def getTSDataDaily(stockDat, t):
+        try:
+            e = stockDat[t.strftime("%Y-%m-%d")]
+            return {"open": round(float(e["1. open"]), 2),
+                    "close": round(float(e["4. close"]), 2),
+                    "high": round(float(e["2. high"]), 2),
+                    "low": round(float(e["3. low"]), 2),
+                    "volume": int(e["5. volume"])}
+        except KeyError:
+            return False
+
+    def getTSDataDailyForceSuccessFuture(stockDat, t, maxFail):
+        y = getTSDataDaily(stockDat, t)
+        i = 0
+        while not y:
+            t += datetime.timedelta(days=1)
+            y = getTSDataDaily(stockDat, t)
+            i += 1
+            if i == maxFail:
+                return False
+
+        return (t, y)
+
     dailyDat = rqstStockTSDataDaily(sym)
     dat = read_articles()
     for article in dat:
         # round down to nearest hour
         t = (datetime.datetime.strptime(
             article["date_published"], "%Y-%m-%dT%H:%M:%SZ") - datetime.timedelta(hours=5)).replace(second=0, minute=0)
-#            print(dailyDat)
+        #            print(dailyDat)
         result = getTSDataDailyForceSuccessFuture(dailyDat, t, 1000)
         if not result:
             article["delta"] = float(0)
