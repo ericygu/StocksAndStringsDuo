24f2e0870698a193ac27a0f4eeb5a56c4b0d4b66
ericygu <gudmaneric@gmail.com>
2020-11-27 15:42:15 -0600

    Bug fixing
    Added Libraries, fixed typos in methods

--- preprocessing.py
+++ preprocessing.py
@@ -5,11 +5,13 @@ from sklearn import preprocessing
 from sklearn import model_selection as ms
 from form_csv import json_to_df
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def normalization(xTrain, xTest):
     scaler = preprocessing.StandardScaler()
     xTrain = scaler.fit_transform(xTrain)
-    xTest = scaler.trasnform(xTest)
+    xTest = scaler.transform(xTest)
     return xTrain, xTest
 
 # Unedited, can you unnormalized data
@@ -23,7 +25,7 @@ def pearson_graph(dfx, dfy):
     plt.show()
     return None
 
-def process():
+def process(x, y):
     # Acquire dataset
     # Don't run this again, it generates new data and replaces what we have. We will have to write something that adds to current data later.
     # I took care of reading from the current data files and converting them to dataframes/csv. -Nathan
@@ -41,8 +43,8 @@ def process():
 
     n = 5194 # number of keywords to include in the dataset
     x, y = json_to_df(n)
-	x.to_csv("x")
-	y.to_csv("y")
+    x.to_csv("x")
+    y.to_csv("y")
 
     xTrain, xTest, yTrain, yTest = ms.train_test_split(x, y, test_size = 0.2)
     x_norm = normalization(x)
