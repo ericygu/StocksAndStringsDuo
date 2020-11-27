8d798615e57cc103de49fdacb71bee35c796bfe7
ericygu <gudmaneric@gmail.com>
2020-11-27 15:44:27 -0600

    Formatting
    scaler to std_scale, spacing for nice code

--- preprocessing.py
+++ preprocessing.py
@@ -8,11 +8,13 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def normalization(xTrain, xTest):
-    scaler = preprocessing.StandardScaler()
-    xTrain = scaler.fit_transform(xTrain)
-    xTest = scaler.transform(xTest)
+    std_scale = preprocessing.StandardScaler()
+    xTrain = std_scale.fit_transform(xTrain)
+    xTest = std_scale.transform(xTest)
     return xTrain, xTest
+
 
 # Unedited, can you unnormalized data
 def pearson_graph(dfx, dfy):
@@ -20,10 +22,11 @@ def pearson_graph(dfx, dfy):
     df['target'] = pd.Series(dfy['label'])
     correlation_matrix = df.corr(method='pearson')
     # sns.heatmap(correlation_matrix, annot=True)
-    fig, ax = plt.subplots(figsize=(10,10))
+    fig, ax = plt.subplots(figsize=(10, 10))
     sns.heatmap(correlation_matrix)
     plt.show()
     return None
+
 
 def process(x, y):
     # Acquire dataset
@@ -41,21 +44,22 @@ def process(x, y):
     exec(open("./form_csv.py").read())
     """
 
-    n = 5194 # number of keywords to include in the dataset
+    n = 5194  # number of keywords to include in the dataset
     x, y = json_to_df(n)
     x.to_csv("x")
     y.to_csv("y")
 
-    xTrain, xTest, yTrain, yTest = ms.train_test_split(x, y, test_size = 0.2)
+    xTrain, xTest, yTrain, yTest = ms.train_test_split(x, y, test_size=0.2)
     x_norm = normalization(x)
-    
+
     xTrain.to_csv("xTrain")
     xTest.to_csv("xTest")
     yTrain.to_csv("yTrain")
     yTest.to_csv("yTest")
-    
+
     print("Preprocessing Done")
     return None
+
 
 # testing purposes
 """
@@ -66,8 +70,3 @@ def main():
 if __name__ == '__main__':
     main()
 """
-    
-
-
-
-
