7b33e93e6732e86cb76a34420904e577198fb607
food88 <f8o8o8d@gmail.com>
2020-12-05 20:16:18 -0500

    Update preprocessing.py
    Add datetime processing and implemented pearson correlation graph

--- preprocessing.py
+++ preprocessing.py
@@ -15,8 +15,17 @@ def normalization(xTrain, xTest):
     xTest = std_scale.transform(xTest)
     return xTrain, xTest
 
+def extract_features(df):
+    df['date'] = pd.to_datetime(df['date'])
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month
+    df['day'] = df['date'].dt.day
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    df = df.drop(columns=['date'])
+    return df
 
-# Unedited, can you unnormalized data
+# no need for normalized data with pearson correlation graph
 def pearson_graph(dfx, dfy):
     df = dfx.copy(deep=True)
     df['target'] = pd.Series(dfy['label'])
@@ -28,7 +37,7 @@ def pearson_graph(dfx, dfy):
     return None
 
 
-def process(x, y):
+def process():
     # Acquire dataset
     # Don't run this again, it generates new data and replaces what we have. We will have to write something that adds to current data later.
     # I took care of reading from the current data files and converting them to dataframes/csv. -Nathan
@@ -43,19 +52,36 @@ def process(x, y):
     # Turning json data to csv data in either numpy format or df format
     exec(open("./form_csv.py").read())
     """
-
+    # convert data from json to dataframe
     n = 5194  # number of keywords to include in the dataset
     x, y = json_to_df(n)
-    x.to_csv("x")
-    y.to_csv("y")
 
+    # extract features of datetime column
+    x = extract_features(x)
+    # drop datetime column from y data
+    y = y.drop(columns=['date'])
+
+    # split data into training set and test set
     xTrain, xTest, yTrain, yTest = ms.train_test_split(x, y, test_size=0.2)
-    x_norm = normalization(x)
 
-    xTrain.to_csv("xTrain")
-    xTest.to_csv("xTest")
-    yTrain.to_csv("yTrain")
-    yTest.to_csv("yTest")
+    """
+    xTrain.to_csv("xTrain_orig", index=False)
+    xTest.to_csv("xTest_orig", index=False)
+    yTrain.to_csv("yTrain_orig", index=False)
+    yTest.to_csv("yTest_orig", index=False)
+    """
+
+    # normalize the x data
+    xTrain, xTest = normalization(xTrain, xTest)
+
+    # Pearson graph of features with datetime extracted (can be commented out to not show pearson correlation graph)
+    pearson_graph(xTrain, yTrain)
+
+    # convert dataframes to csv files
+    xTrain.to_csv("xTrain", index=False)
+    xTest.to_csv("xTest", index=False)
+    yTrain.to_csv("yTrain", index=False)
+    yTest.to_csv("yTest", index=False)
 
     print("Preprocessing Done")
     return None
@@ -64,8 +90,7 @@ def process(x, y):
 # testing purposes
 """
 def main():
-    xTrain, yTrain, xTest, yTest = process()
-    pearson_graph()
+    process()
 
 if __name__ == '__main__':
     main()
