a1d817c45c081ebb83c11808975271249ecb5f7b
Nathan Yang <65429172+Algernonagon@users.noreply.github.com>
2020-12-06 19:33:49 -0500

    finished preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -10,7 +10,7 @@ import seaborn as sns
 
 
 def normalization(xTrain, xTest):
-    std_scale = preprocessing.StandardScaler()
+    std_scale = preprocessing.StandardScaler(with_mean=False)
     cols = list(xTrain.columns)
     xTrain = std_scale.fit_transform(xTrain)
     xTest = std_scale.transform(xTest)
@@ -27,25 +27,36 @@ def extract_features(df):
     df['weekday'] = df['datetime'].dt.dayofweek
     df['hour'] = df['datetime'].dt.hour
     """
-    df.insert(loc=0, column='year', value=df['datetime'].dt.year, allow_duplicates=True)
-    df.insert(loc=0, column='month', value=df['datetime'].dt.month, allow_duplicates=True)
-    df.insert(loc=0, column='day', value=df['datetime'].dt.day, allow_duplicates=True)
-    df.insert(loc=0, column='dayofweek', value=df['datetime'].dt.dayofweek, allow_duplicates=True)
-    df.insert(loc=0, column='hour', value=df['datetime'].dt.hour, allow_duplicates=True)
+    df.insert(loc=0, column='dt_year', value=df['datetime'].dt.year)
+    df.insert(loc=0, column='dt_month', value=df['datetime'].dt.month)
+    df.insert(loc=0, column='dt_day', value=df['datetime'].dt.day)
+    df.insert(loc=0, column='dt_dayofweek', value=df['datetime'].dt.dayofweek)
+    df.insert(loc=0, column='dt_hour', value=df['datetime'].dt.hour)
     df = df.drop(columns=['datetime'])
     return df
 
-# no need for normalized data with pearson correlation graph
+# use pearson correlation to remove redundant features and features with nan correlation to stock price change
 def pearson_graph(dfx, dfy):
     df = dfx.copy(deep=True)
     #df['target'] = dfy['stock_change']
-    df.insert(loc=0, column='target', value=dfy['stock_change'], allow_duplicates=True)
-    correlation_matrix = df.corr(method='pearson')
+    df.insert(loc=0, column='y_target', value=dfy['stock_change'])
+    corr = df.corr(method='pearson')
     # sns.heatmap(correlation_matrix, annot=True)
     #fig, ax = plt.subplots(figsize=(10, 10))
     #sns.heatmap(correlation_matrix)
     #plt.show()
-    return None
+    columns = np.full((corr.shape[0],), True, dtype=bool)
+    for i in tqdm(range(corr.shape[0])):
+        if pd.isnull(corr.iloc[0,i]):
+            if columns[i]:
+                columns[i] = False
+        else:
+            for j in range(i+1, corr.shape[0]):
+                if corr.iloc[i,j] >= 0.9:
+                    if columns[j]:
+                        columns[j] = False
+    selected_columns = df.columns[columns]
+    return selected_columns, corr
 
 
 def process():
@@ -76,26 +87,28 @@ def process():
 
     # split data into training set and test set
     xTrain, xTest, yTrain, yTest = ms.train_test_split(x, y, test_size=0.2)
-
-    """
-    xTrain.to_csv("xTrain_orig", index=False)
-    xTest.to_csv("xTest_orig", index=False)
-    yTrain.to_csv("yTrain_orig", index=False)
-    yTest.to_csv("yTest_orig", index=False)
-    """
+    xTrain.to_csv("xTrain_base.csv", index=False)
+    xTest.to_csv("xTest_base.csv", index=False)
+    yTrain.to_csv("yTrain.csv", index=False)
+    yTest.to_csv("yTest.csv", index=False)
 
     # Pearson graph of features with datetime extracted (can be commented out to not show pearson correlation graph)
-    pearson_graph(xTrain, yTrain)
+    selected_columns, corr = pearson_graph(xTrain, yTrain)
+    corr.to_csv("corr_matrix.csv")
 
     # normalize the x data
     xTrain, xTest = normalization(xTrain, xTest)
 
+    # remove reduntant features
+    xTrain_pearson = xTrain[selected_columns[1:]]
+    xTest_pearson = xTest[selected_columns[1:]]
+
     # convert dataframes to csv files
     xTrain.to_csv("xTrain.csv", index=False)
+    xTrain_pearson.to_csv("xTrain_pearson.csv", index=False)
     xTest.to_csv("xTest.csv", index=False)
-    yTrain.to_csv("yTrain.csv", index=False)
-    yTest.to_csv("yTest.csv", index=False)
-
+    xTest_pearson.to_csv("xTest_pearson.csv", index=False)
+    
     print("Preprocessing Done")
     return None
 
